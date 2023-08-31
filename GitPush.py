bbd50dea9b99fd144bd3d21bcb325b4b68f76038
MatthCorn <matthieucornu@yahoo.fr>
2023-08-31 22:34:52 +0200

    test push script

--- GitPush.py
+++ GitPush.py
@@ -1,16 +1,16 @@
 from git import Repo
 
-PATH_OF_GIT_REPO = r'path\to\your\project\folder\.git'  # make sure .git folder is properly configured
-COMMIT_MESSAGE = 'comment from python script'
+PATH_OF_GIT_REPO = r'C:\Users\matth\Documents\Python\Projets\.git'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'test push script'
 
-def git_push():
+def git_push(file):
     try:
         repo = Repo(PATH_OF_GIT_REPO)
-        repo.git.add(update=True)
+        repo.git.add([file], update=True)
         repo.index.commit(COMMIT_MESSAGE)
         origin = repo.remote(name='origin')
         origin.push()
     except:
         print('Some error occured while pushing the code')
 
-git_push()
+git_push(r'C:\Users\matth\Documents\Python\Projets\GitPush.py')
