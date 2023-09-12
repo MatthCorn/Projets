66c1a485691ebcf4fc0c6fae5acb892c3340326c
MatthCorn <matthieucornu@yahoo.fr>
2023-09-12 17:18:38 +0200

    simu Order

--- OrderDetector/Trainer2.py
+++ OrderDetector/Trainer2.py
@@ -2,69 +2,90 @@ import numpy as np
 
 from OrderDetector.Detector import Network
 from OrderDetector.DataMaker import MakeData
-from math import sqrt
+from math import sqrt, log10
 import torch
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-N = Network(n_encoder=5, d_in=10, d_model=256, d_att=256, WidthsEmbedding=[32, 64], num_heads=16, relative=True, masked=False)
+N = Network(n_encoder=5, d_in=10, d_model=64, d_att=64, WidthsEmbedding=[32], num_heads=4, relative=True, masked=False)
 N.to(device)
 
 
-optimizer = torch.optim.Adam(N.parameters(), weight_decay=1e-3, lr=1e-3)
+optimizer = torch.optim.Adam(N.parameters(), weight_decay=1e-3, lr=3e-4)
 
-ValidationInput, ValidationOutput, ValidationShift = MakeData(NVec=5, DVec=10, sigma=1, NData=10000, ShiftInterval=[0, 1000])
+Max = 10000
+Min = 0
+
+NDataT = 10000
+NDataV = 1000
+ValidationInput, ValidationOutput, _ = MakeData(NVec=5, DVec=10, sigma=1, NData=NDataV, ShiftInterval=[Min, Max])
 
 batch_size = 10000
 n_batch = 1
 
-n_iter = 2000
+n_iter = 1000
 TrainingError = []
 ValidationError = []
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 
-ShiftList = 50*np.array(list(range(20)))
+# ShiftList = 20*np.array(list(range(50)))
+# ShiftIntervals = [[ShiftList[i], ShiftList[i+1]] for i in range(len(ShiftList)-1)] + [[0, 200]] + [[0, 1000]] + [[0, 1000]]
 
-for p in range(1):
-    for i in range(len(ShiftList)-1):
-        print(i)
-        TrainingInput, TrainingOutput, _ = MakeData(NVec=5, DVec=10, sigma=1, NData=10000, ShiftInterval=[ShiftList[i], ShiftList[i+1]])
 
-        for j in tqdm(range(n_iter)):
-            error = 0
-            for k in range(n_batch):
-                optimizer.zero_grad(set_to_none=True)
+ShiftList = [0] + list(np.logspace(1, log10(Max), 10))
+ShiftList = list(map(int, ShiftList))
+ShiftIntervals = [[ShiftList[i], ShiftList[i+1]] for i in range(len(ShiftList)-1)] + [[Min, Max]]
 
-                InputBatch = TrainingInput[k*batch_size:(k+1)*batch_size].to(device)
-                OutputBatch = TrainingOutput[k*batch_size:(k+1)*batch_size].to(device)
-                Prediction = N(InputBatch)
 
-                err = torch.norm(OutputBatch-Prediction, p=2)
-                err.backward()
-                optimizer.step()
+for ShiftInterval in ShiftIntervals:
+    TrainingInput, TrainingOutput, _ = MakeData(NVec=5, DVec=10, sigma=1, NData=NDataT, ShiftInterval=ShiftInterval)
 
-                error += float(err)
+    if ShiftInterval == [Min, Max]:
+        n_iter = 3000
 
-            with torch.no_grad():
-                InputBatch = ValidationInput.to(device)
-                OutputBatch = ValidationOutput.to(device)
-                Prediction = N(InputBatch)
+    for j in tqdm(range(n_iter)):
+        error = 0
+        for k in range(n_batch):
+            optimizer.zero_grad(set_to_none=True)
 
-                err = torch.norm(OutputBatch - Prediction, p=2)
-                ValidationError.append(float(err)/sqrt(10000*5))
+            InputBatch = TrainingInput[k*batch_size:(k+1)*batch_size].to(device)
+            OutputBatch = TrainingOutput[k*batch_size:(k+1)*batch_size].to(device)
+            Prediction = N(InputBatch)
 
-            TrainingError.append(error/sqrt(10000*5))
+            err = torch.norm(OutputBatch-Prediction, p=2)
+            err.backward()
+            optimizer.step()
 
-        ErrShift = torch.norm(OutputBatch - Prediction, dim=(1, 2)).cpu().numpy()/sqrt(5)
-        Shift = ValidationShift[:, 0, 0].numpy()
+            error += float(err)
 
-        e, d, c, b, a = np.polyfit(Shift, ErrShift, deg=4)
+        with torch.no_grad():
+            InputBatch = ValidationInput.to(device)
+            OutputBatch = ValidationOutput.to(device)
+            Prediction = N(InputBatch)
 
-        x = np.linspace(0, ShiftList[-1], 100)
-        y = a + b*x + c*x**2 + d*x**3 + e*x**4
-        ax2.plot(x, y, label=str(ShiftList[i+1] + p))
+            err = torch.norm(OutputBatch - Prediction, p=2)
+            ValidationError.append(float(err)/sqrt(NDataV*5))
+
+        TrainingError.append(error/sqrt(NDataT*5))
+
+    Input, Output, Shift = MakeData(NVec=5, DVec=10, sigma=1, NData=10000, ShiftInterval=[Min, Max])
+
+    with torch.no_grad():
+        Input = Input.to(device)
+        Output = Output.to(device)
+        Prediction = N(Input)
+        ErrShift = torch.norm(Output - Prediction, dim=(1, 2)).cpu().numpy() / sqrt(5)
+    Shift = Shift[:, 0, 0].numpy()
+
+    #g, f, \
+    e, d, c, b, a = np.polyfit(Shift, ErrShift, deg=4)
+
+    x = np.linspace(0, ShiftList[-1], 100)
+    y = a + b * x + c * x ** 2 + d * x ** 3 + e * x ** 4 #+ f * x ** 5 + g * x ** 6
+    ax2.plot(x, y, label=str(ShiftInterval[1]))
+
 
 
 ax1.plot(TrainingError, 'r', label="Ensemble d'entrainement")
