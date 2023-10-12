b0dadbdda9a6c3584bbbc4ab9214acc7c168053c
MatthCorn <matthieucornu@yahoo.fr>
2023-10-12 17:23:15 +0200

    taf du jour

--- Complete/Network.py
+++ Complete/Network.py
@@ -1,127 +1,87 @@
-import math
 import torch
 import torch.nn as nn
-from Transformer.EncoderTransformer import EncoderLayer
-from Transformer.EasyFeedForward import FeedForward
-from Transformer.DecoderTransformer import DecoderLayer
-from FDTTranslator.Bursts.TrackerInspired.Embedding import TrackerEmbeddingLayer
-def MakePermutMask(n_tracker, target_len):
-    Permut = (torch.tril(torch.ones(n_tracker * target_len, n_tracker * target_len), n_tracker) -
-              torch.tril(torch.ones(n_tracker * target_len, n_tracker * target_len), n_tracker - 1) +
-              torch.tril(torch.ones(n_tracker * target_len, n_tracker * target_len), -n_tracker * (target_len - 1)) -
-              torch.tril(torch.ones(n_tracker * target_len, n_tracker * target_len), -n_tracker * (target_len - 1) - 1))
-    Powers = tuple(torch.linalg.matrix_power(Permut, i) for i in range(target_len))
+from Complete.Transformer.EncoderTransformer import EncoderLayer
+from Complete.Transformer.EasyFeedForward import FeedForward
+from Complete.Transformer.DecoderTransformer import DecoderLayer
+from Complete.Embedding import TrackerEmbeddingLayer
+from Complete.PositionalEncoding import ClassicPositionalEncoding
+
+def MakePermutMask(n_tracker, len_target):
+    Permut = (torch.tril(torch.ones(n_tracker * len_target, n_tracker * len_target), n_tracker) -
+              torch.tril(torch.ones(n_tracker * len_target, n_tracker * len_target), n_tracker - 1) +
+              torch.tril(torch.ones(n_tracker * len_target, n_tracker * len_target), -n_tracker * (len_target - 1)) -
+              torch.tril(torch.ones(n_tracker * len_target, n_tracker * len_target), -n_tracker * (len_target - 1) - 1))
+    Powers = tuple(torch.linalg.matrix_power(Permut, i) for i in range(len_target))
     return torch.tril(sum(Powers)).unsqueeze(0).unsqueeze(0)
 
-def MakeTrackerMask(n_tracker, target_len):
-    mask = torch.zeros(n_tracker*target_len, n_tracker*target_len)
-    for i in range(target_len):
+def MakeTrackerMask(n_tracker, len_target):
+    mask = torch.zeros(n_tracker*len_target, n_tracker*len_target)
+    for i in range(len_target):
         mask[i*n_tracker:, i*n_tracker:(i+1)*n_tracker] = 1
     return mask.unsqueeze(0).unsqueeze(0)
 
 
 class TransformerTranslator(nn.Module):
 
-    def __init__(self, d_pulse, d_PDW, d_latent=32, num_heads=4, num_encoders=3, n_tracker=4,
-                 num_decoders=3, NbPDWsMemory=10, target_len=20, num_flags=4, device=torch.device('cpu'), FPIC=False):
+    def __init__(self, d_pulse, d_PDW, d_att=32, n_heads=4, n_encoders=3, n_tracker=4,
+                 n_decoders=3, n_PDWs_memory=10, len_target=20, n_flags=4, device=torch.device('cpu')):
         super().__init__()
         self.device = device
         self.d_PDW = d_PDW
-        self.d_latent = d_latent
+        self.d_att = d_att
         self.n_tracker = n_tracker
-        self.num_flags = num_flags
+        self.n_flags = n_flags
 
-        self.Encoders = nn.ModuleList()
-        for i in range(num_encoders):
-            self.Encoders.append(EncoderLayer(d_model=d_latent, d_att=d_latent, num_heads=num_heads, relative=False, MHADropout=0, FFDropout=0))
+        self.encoders = nn.ModuleList()
+        for i in range(n_encoders):
+            self.encoders.append(EncoderLayer(d_att=d_att, n_heads=n_heads))
 
-        self.Decoders = nn.ModuleList()
-        Decoder = DecoderLayer(d_model=d_latent, d_att=d_latent, num_heads=num_heads, target_len=target_len*n_tracker, relative=False, MHADropout=0, FFDropout=0)
-        Decoder.SelfAttentionLayer.mask = MakePermutMask(n_tracker, target_len)
-        self.Decoders.append(Decoder)
-        for i in range(num_decoders-1):
-            Decoder = DecoderLayer(d_model=d_latent, d_att=d_latent, num_heads=num_heads, target_len=target_len*n_tracker, relative=False, MHADropout=0, FFDropout=0)
-            Decoder.SelfAttentionLayer.mask = MakeTrackerMask(n_tracker, target_len)
-            self.Decoders.append(Decoder)
+        self.decoders = nn.ModuleList()
+        for i in range(n_decoders):
+            self.decoders.append(DecoderLayer(d_att=d_att, n_heads=n_heads))
 
-        self.NbPDWsMemory = NbPDWsMemory
-        self.FPIC = FPIC
+        self.n_PDWs_memory = n_PDWs_memory
 
+        self.source_embedding = FeedForward(d_in=d_pulse, d_out=d_att, widths=[], dropout=0)
+        self.target_embedding = FeedForward(d_in=d_PDW+n_flags, d_out=d_att, widths=[], dropout=0)
+        self.tracker_embedding = TrackerEmbeddingLayer(n_tracker=n_tracker, d_att=d_att)
 
+        self.physical_prediction = FeedForward(d_in=d_att, d_out=d_PDW, widths=[16], dropout=0)
+        self.flags_prediction = FeedForward(d_in=d_att, d_out=n_flags, widths=[8], dropout=0)
+        self.action_prediction = FeedForward(d_in=d_att, d_out=1, widths=[8], dropout=0)
 
-        self.SourceEmbedding = FeedForward(d_in=d_pulse, d_out=d_latent, widths=[], dropout=0)
-        self.TargetEmbedding = FeedForward(d_in=d_PDW+num_flags, d_out=d_latent, widths=[], dropout=0)
-        self.TrackerEmbedding = TrackerEmbeddingLayer(n_tracker=n_tracker, d_input_decoder=d_latent, d_att=d_latent)
-
-        self.PhysicalPrediction = FeedForward(d_in=d_latent, d_out=d_PDW, widths=[16], dropout=0)
-        self.FlagsPrediction = FeedForward(d_in=d_latent, d_out=num_flags, widths=[8], dropout=0)
-
-        # Ce vecteur a pour but de déterminer l'action a réaliser, mettre fin à la traduction dans notre cas particulier
-        self.ActionPrediction = FeedForward(d_in=d_latent, d_out=1, widths=[8], dropout=0)
-
-        if self.FPIC:
-            # Il faut donner à ce classifieur l'impulsion fictive transportée dans l'espace des PDWs
-            self.PulseToPDWs = FeedForward(d_in=d_latent, d_out=d_latent, widths=[], dropout=0)
-
-        self.PEEncoder = PositionalEncoding(d_model=d_latent, dropout=0, max_len=target_len, device=device)
-        self.PEDecoder = PositionalEncoding(d_model=d_latent, dropout=0, max_len=target_len, device=device)
+        self.PE_encoder = ClassicPositionalEncoding(d_model=d_att, dropout=0, max_len=len_target, device=device)
+        self.PE_decoder = ClassicPositionalEncoding(d_model=d_att, dropout=0, max_len=len_target, device=device)
 
         self.to(device)
 
     def forward(self, source, target):
-        # target.shape = (batch_size, target_len, d_target+num_flags)
+        # target.shape = (batch_size, len_target, d_target+num_flags)
         batch_size, len_source, _ = source.shape
 
-        trg = self.PEDecoder(self.TargetEmbedding(target))
-        # trg.shape = (batch_size, target_len, d_latent)
-        trg = self.TrackerEmbedding(trg)
-        # trg.shape = (batch_size, n_tracker*target_len, d_latent)
+        trg = self.PE_decoder(self.target_embedding(target))
+        # trg.shape = (batch_size, len_target, d_att)
+        trg = self.tracker_embedding(trg)
+        # trg.shape = (batch_size, n_tracker*len_target, d_att)
 
-        src = self.PEEncoder(self.SourceEmbedding(source))
-        # src.shape = (batch_size, len_source, d_latent)
-
-        if self.FPIC:
-            src, FakePulse = torch.split(src, len_source - 1, dim=1)
+        src = self.PE_encoder(self.source_embedding(source))
+        # src.shape = (batch_size, len_source, d_att)
 
         for Encoder in self.Encoders:
             src = Encoder(src)
 
         for Decoder in self.Decoders:
             trg = Decoder(target=trg, source=src)
-        # trg.shape = (batch_size, n_tracker*target_len, d_latent)
+        # trg.shape = (batch_size, n_tracker*len_target, d_att)
 
-        trg = trg.reshape(batch_size, -1, self.n_tracker*self.d_latent)[:, :, :self.d_latent]
-        # trg.shape = (batch_size, target_len, d_latent)
-
-        if self.FPIC:
-            Physic, Flags, Action = self.PhysicalPrediction(trg), self.FlagsPrediction(trg), self.ActionPrediction(trg - self.PulseToPDWs(FakePulse))
-        else:
-            Physic, Flags, Action = self.PhysicalPrediction(trg), self.FlagsPrediction(trg), self.ActionPrediction(trg)
-
-        # Physic.shape = (batch_size, target_len, d_PDW)
-        # Flags.shape = (batch_size, target_len, num_flags)
-        # Action.shape = (batch_size, target_len, 1)
-        PDW = torch.cat((Physic, Flags), dim=2)
-        return PDW, Action
+        trg = trg.reshape(batch_size, -1, self.n_tracker*self.d_att)[:, :, :self.d_att]
+        # trg.shape = (batch_size, len_target, d_att)
 
 
-class PositionalEncoding(nn.Module):
+        physic, flags, action = self.physical_prediction(trg), self.flags_prediction(trg), self.action_prediction(trg)
 
-    def __init__(self, d_model: int, dropout: float = 0., max_len: int = 5000, device=torch.device('cpu')):
-        super().__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        position = torch.arange(max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        pe = torch.zeros(1, max_len, d_model)
-        pe[0, :, 0::2] = torch.sin(position * div_term)
-        pe[0, :, 1::2] = torch.cos(position * div_term)
-        self.register_buffer('pe', pe.to(device=device))
-
-    def forward(self, x):
-        """
-        Arguments:
-            x: Tensor, shape ``[batch_size, seq_len, embedding_dim]``
-        """
-        x = x + self.pe[:, :x.size(1)]
-        return self.dropout(x)
+        # Physic.shape = (batch_size, len_target, d_PDW)
+        # Flags.shape = (batch_size, len_target, n_flags)
+        # Action.shape = (batch_size, len_target, 1)
+        PDW = torch.cat((physic, flags), dim=2)
+        return PDW, action
---
+++ Complete/PositionalEncoding.py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn
+import math
+
+class ClassicPositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0., max_len: int = 5000, device=torch.device('cpu')):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(1, max_len, d_model)
+        pe[0, :, 0::2] = torch.sin(position * div_term)
+        pe[0, :, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe.to(device=device))
+
+    def forward(self, x):
+        """
+        Arguments:
+            x: Tensor, shape ``[batch_size, seq_len, embedding_dim]``
+        """
+        x = x + self.pe[:, :x.size(1)]
+        return self.dropout(x)
+
+
+class RotaryPositionalEncoding(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+class Rotary(torch.nn.Module):
+    def __init__(self, dim, base=10000):
+        super().__init__()
+        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float() / dim))
+        self.register_buffer("inv_freq", inv_freq)
+        self.seq_len_cached = None
+        self.cos_cached = None
+        self.sin_cached = None
+
+    def forward(self, x, seq_dim=1):
+        seq_len = x.shape[seq_dim]
+        if seq_len != self.seq_len_cached:
+            self.seq_len_cached = seq_len
+            t = torch.arange(x.shape[seq_dim], device=x.device).type_as(self.inv_freq)
+            freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+            emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
+            self.cos_cached = emb.cos()[:, None, None, :]
+            self.sin_cached = emb.sin()[:, None, None, :]
+        return self.cos_cached, self.sin_cached
+
+
+# rotary pos emb helpers:
+
+def rotate_half(x):
+    x1, x2 = x[..., : x.shape[-1] // 2], x[..., x.shape[-1] // 2 :]
+    return torch.cat(
+        (-x2, x1), dim=x1.ndim - 1
+    )  # dim=-1 triggers a bug in torch < 1.8.0
+
+
+@torch.jit.script
+def apply_rotary_pos_emb(q, k, cos, sin):
+    return (q * cos) + (rotate_half(q) * sin), (k * cos) + (rotate_half(k) * sin)
