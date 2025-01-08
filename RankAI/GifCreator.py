f8cc24e55ddc22827ae9d23977729b26bf39af22
MatthCorn <matthieucornu@yahoo.fr>
2025-01-08 16:05:09 +0100

    correction distrib et ajout échelle et colorbar

--- RankAI/GifCreator.py
+++ RankAI/GifCreator.py
@@ -1,14 +1,19 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
-import math
+import numpy as np
 
 import os
 
 def MakeGIF(PlottingData, NData, training_strategy, distrib, save_path):
     if distrib == 'uniform':
         f = lambda x: x
+        g = lambda x: x
     elif distrib == 'log':
-        f = lambda x: math.log(x)
+        f = lambda x: np.log(x)
+        g = lambda x: np.exp(x)
+    elif distrib == 'exp':
+        f = lambda x: np.exp(x)
+        g = lambda x: np.log(x)
 
     frames = len(PlottingData[0])
 
@@ -17,6 +22,12 @@ def MakeGIF(PlottingData, NData, training_strategy, distrib, save_path):
     std_min = min([window['std'][0] for window in training_strategy])
     std_max = max([window['std'][1] for window in training_strategy])
 
+
+    log_x_ticks = g(np.linspace(f(std_min), f(std_max), 10))
+    x_ticks = np.linspace(0, NData+1, 10)
+    log_y_ticks = g(np.linspace(f(mean_min), f(mean_max), 10))
+    y_ticks = np.linspace(0, NData+1, 10)
+
     # Prepare the figure
     fig, ax = plt.subplots(1, 2, figsize=(8, 4))
 
@@ -24,12 +35,29 @@ def MakeGIF(PlottingData, NData, training_strategy, distrib, save_path):
     def update(frame):
         ax[0].clear()
         ax[1].clear()
+
+        ax[0].set_xticks(x_ticks)
+        ax[0].set_xticklabels([f"{val:.1f}" for val in log_x_ticks])
+        ax[0].set_yticks(y_ticks)
+        ax[0].set_yticklabels([f"{val:.1f}" for val in log_y_ticks])
+
+        ax[1].set_xticks(x_ticks)
+        ax[1].set_xticklabels([f"{val:.1f}" for val in log_x_ticks])
+        ax[1].set_yticks(y_ticks)
+        ax[1].set_yticklabels([f"{val:.1f}" for val in log_y_ticks])
+
+        ax[0].set_xlabel("standard deviation")
+        ax[0].set_ylabel("mean")
+
+        ax[1].set_xlabel("standard deviation")
+        ax[1].set_ylabel("mean")
+
         ax[0].set_title("error :" + str(frame))
         ax[1].set_title("perf :" + str(frame))
         error_im = PlottingData[0][frame].reshape(NData, NData).tolist()
         perf_im = PlottingData[1][frame].reshape(NData, NData).tolist()
-        ax[0].imshow(error_im, cmap="cool", vmin=0, vmax=10)
-        ax[1].imshow(perf_im, cmap="cool", vmin=0, vmax=5)
+        im0 = ax[0].imshow(error_im, cmap="cool", vmin=0, vmax=3)
+        im1 = ax[1].imshow(perf_im, cmap="cool", vmin=0, vmax=1)
 
         current_strat = training_strategy[int(frame/frames * len(training_strategy))]
         current_mean_min = current_strat['mean'][0]
@@ -40,14 +68,14 @@ def MakeGIF(PlottingData, NData, training_strategy, distrib, save_path):
         alpha_mean_max = (f(current_mean_max) - f(mean_max)) / (f(mean_min) - f(mean_max))
         alpha_std_min = (f(current_std_min) - f(std_max)) / (f(std_min) - f(std_max))
         alpha_std_max = (f(current_std_max) - f(std_max)) / (f(std_min) - f(std_max))
-        square_x = [
+        square_y = [
             NData * (1 - alpha_mean_min) - 0.5,
             NData * (1 - alpha_mean_max) - 0.5,
             NData * (1 - alpha_mean_max) - 0.5,
             NData * (1 - alpha_mean_min) - 0.5,
             NData * (1 - alpha_mean_min) - 0.5
         ]
-        square_y = [
+        square_x = [
             NData * (1 - alpha_std_min) - 0.5,
             NData * (1 - alpha_std_min) - 0.5,
             NData * (1 - alpha_std_max) - 0.5,
@@ -56,6 +84,14 @@ def MakeGIF(PlottingData, NData, training_strategy, distrib, save_path):
         ]
         ax0 = ax[0].plot(square_x, square_y, 'r', linewidth=5)
         ax1 = ax[1].plot(square_x, square_y, 'r', linewidth=5)
+
+        if not hasattr(update, "cbar0"):
+            update.cbar0 = plt.colorbar(im0, ax=ax[0])
+            update.cbar1 = plt.colorbar(im1, ax=ax[1])
+        else:
+            update.cbar0.update_normal(im0)
+            update.cbar1.update_normal(im1)
+
         return ax0, ax1
 
     # Create animation
--- RankAI/V0/Indices/DataMaker.py
+++ RankAI/V0/Indices/DataMaker.py
@@ -22,38 +22,36 @@ def MakeTargetedData(NVec=5, DVec=10, mean_min=1e-1, mean_max=1e1, sigma_min=1e0
         Weight = torch.tensor([1.] * DVec)
     Weight = Weight / torch.norm(Weight)
     if plot:
-        if distrib == 'uniform':
-            f = lambda x: x
-        elif distrib == 'log':
-            f = lambda x: math.log(x)
-        mean = torch.exp((f(mean_max) - f(mean_min)) * torch.linspace(0, 1, NData) + f(mean_min))
-        sigma = torch.exp((f(sigma_max) - f(sigma_min)) * torch.linspace(0, 1, NData) + f(sigma_min))
-
-        mean, sigma = torch.meshgrid(mean, sigma)
-        alpha = torch.normal(0, 1, (NData * NData, NVec)) * sigma.reshape(-1, 1) + mean.reshape(-1, 1)
-
-        Input = torch.normal(0, 1, (NData * NData, NVec, DVec)) * sigma.reshape(-1, 1, 1) + mean.reshape(-1, 1, 1)
-        uncontroled_values = torch.matmul(Input, Weight.to(Input.device))
-        Input = Input + (alpha - uncontroled_values).unsqueeze(-1) * Weight.view(1, 1, DVec)
-
-        Output = GetRanks(Input, Weight).unsqueeze(dim=-1)
-        return Input.to(device), Output.to(device)
+        spacing = torch.linspace(0, 1, NData)
     else:
-        if distrib == 'uniform':
-            mean = (mean_max - mean_min) * torch.rand(NData, 1) + mean_min
-            sigma = (sigma_max - sigma_min) * torch.rand(NData, 1) + sigma_min
-        elif distrib == 'log':
-            mean = torch.exp((math.log(mean_max) - math.log(mean_min)) * torch.rand(NData, 1) + math.log(mean_min))
-            sigma = torch.exp((math.log(sigma_max) - math.log(sigma_min)) * torch.rand(NData, 1) + math.log(sigma_min))
-        alpha = torch.normal(0, 1, (NData, NVec)) * sigma + mean
+        spacing = torch.rand(NData)
 
-        Input = torch.normal(0, 1, (NData, NVec, DVec)) * sigma.unsqueeze(-1) + mean.unsqueeze(-1)
-        uncontroled_values = torch.matmul(Input, Weight.to(Input.device))
-        Input = Input + (alpha - uncontroled_values).unsqueeze(-1) * Weight.view(1, 1, DVec)
+    if distrib == 'uniform':
+        f = lambda x: x
+        g = lambda x: x
+    elif distrib == 'log':
+        f = lambda x: math.log(x)
+        g = lambda x: torch.exp(x)
+    elif distrib == 'exp':
+        f = lambda x: math.exp(x)
+        g = lambda x: torch.log(x)
+    mean = g((f(mean_max) - f(mean_min)) * spacing + f(mean_min))
+    sigma = g((f(sigma_max) - f(sigma_min)) * spacing + f(sigma_min))
 
-        Output = GetRanks(Input, Weight).unsqueeze(dim=-1)
-        return Input.to(device), Output.to(device)
+    if plot:
+        mean, sigma = torch.meshgrid(mean, sigma)
+        NData = NData ** 2
 
+    mean, sigma = mean.reshape(-1, 1), sigma.reshape(-1, 1)
+
+    alpha = torch.normal(0, 1, (NData, NVec)) * sigma + mean
+
+    Input = torch.normal(0, 1, (NData, NVec, DVec)) * sigma.unsqueeze(-1) + mean.unsqueeze(-1)
+    uncontroled_values = torch.matmul(Input, Weight.to(Input.device))
+    Input = Input + (alpha - uncontroled_values).unsqueeze(-1) * Weight.view(1, 1, DVec)
+
+    Output = GetRanks(Input, Weight).unsqueeze(dim=-1)
+    return Input.to(device), Output.to(device)
 
 if __name__ == '__main__':
     MakeTargetedData(plot=True)
