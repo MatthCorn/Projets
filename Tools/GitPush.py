277411da9ae711157b23701b70e4ad46c64aa6a6
MatthCorn <matthieucornu@yahoo.fr>
2025-01-29 12:54:22 +0100

    config gitpush

--- Tools/GitPush.py
+++ Tools/GitPush.py
@@ -6,14 +6,14 @@ local = os.path.join(os.path.abspath(__file__)[:(os.path.abspath(__file__).index
 COMMIT_MESSAGE = 'Rien à dire'
 
 def git_push(local, save_path, CommitMsg=COMMIT_MESSAGE):
-    try:
-        repo = Repo(os.path.join(local, r'.git'))
-        repo.git.add([save_path])
-        repo.index.commit(CommitMsg)
-        origin = repo.remote(name='origin')
-        origin.push()
-    except:
-        print('Some error occured while pushing the code')
+    # try:
+    repo = Repo(os.path.join(local, r'.git'))
+    repo.git.add([save_path])
+    repo.index.commit(CommitMsg)
+    origin = repo.remote(name='origin')
+    origin.push()
+    # except:
+    #     print('Some error occured while pushing the code')
 
 if __name__ == '__main__':
     git_push(local=local, file=r'Tools.GitPush.py')
