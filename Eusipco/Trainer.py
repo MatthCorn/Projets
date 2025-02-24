d8d649ddc47db787636157da954d398b924ed979
MatthCorn <matthieucornu@yahoo.fr>
2025-02-24 15:29:20 +0100

    simu + modif gitpush pour debug

--- Eusipco/Trainer.py
+++ Eusipco/Trainer.py
@@ -54,7 +54,7 @@ param = {"n_encoder": 10,
          "batch_size": 1000,
          "n_iter": 130,
          "training_strategy": [
-             {"mean": [-10, 10], "std": [0.001, 1]}
+             {"mean": [-1000, 1000], "std": [0.1, 500]}
          ],
          "distrib": "log",
          "plot_distrib": "log",
