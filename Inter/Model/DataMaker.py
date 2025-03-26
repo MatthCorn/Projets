04ecc24dcdb6285b7d544c1356743bb9a40a4fe9
MatthCorn <matthieucornu@yahoo.fr>
2025-03-26 17:24:55 +0100

    gen data palier

--- Inter/Model/DataMaker.py
+++ Inter/Model/DataMaker.py
@@ -4,20 +4,38 @@ from Tools.XMLTools import loadXmlAsObj, saveObjAsXml
 import os
 from tqdm import tqdm
 
-def MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data, sens_fourier, sens_sensor):
+def generate_sample(args):
+    import warnings
+    warnings.filterwarnings("ignore")  # Désactive les warnings
+
+    """ Génère un échantillon unique basé sur les paramètres. """
+    d_in, n_pulse_plateau, len_in, len_out, sensitivity = args
+
+    S = Simulator(n_pulse_plateau, len_in, d_in - 1, sensitivity=sensitivity)
+    S.Run()
+
+    input_data = S.L
+    output = S.sensor_simulator.R
+    len_element_output = len(output)
+
+    # Remplissage pour correspondre à len_out
+    output += [[0.] * (d_in + 1)] * (len_out - len(output))
+
+    return input_data, output[:len_out], len_element_output
+
+def MakeData1(d_in, n_pulse_plateau, len_in, len_out, n_data, sensitivity):
+    import warnings
+    warnings.filterwarnings("ignore")   # Désactive les warnings
+
     input_data = []
     output_data = []
     len_element_output = []
 
     for _ in tqdm(range(n_data)):
-        S = Simulator(n_pulse_plateau, len_in, d_in - 1, sensitivity_fourier=sens_fourier,
-                      sensitivity_sensor=sens_sensor)
-        S.Run()
-        input_data.append(S.L)
-        output = S.sensor_simulator.R
-        len_element_output.append(len(output))
-        output += [[0.] * (d_in + 1)] * (len_out - len(output))
-        output_data.append(output[:len_out])
+        input, output, len_output = generate_sample((d_in, n_pulse_plateau, len_in, len_out, sensitivity))
+        input_data.append(input)
+        output_data.append(output)
+        len_element_output.append(len_output)
 
     len_element_output = torch.tensor(len_element_output).unsqueeze(-1)
     arange = torch.arange(len_out + 1).unsqueeze(0).expand(n_data, -1)
@@ -26,18 +44,45 @@ def MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data, sens_fourier, sens_
 
     return torch.tensor(input_data, dtype=torch.float), torch.tensor(output_data, dtype=torch.float), [add_mask, mult_mask]
 
-def GetData(d_in, n_pulse_plateau, len_in, len_out, n_data_training, n_data_validation, sens_fourier, sens_sensor, save_path=None):
+from concurrent.futures import ProcessPoolExecutor
+
+def MakeData2(d_in, n_pulse_plateau, len_in, len_out, n_data, sensitivity):
+    """ Génère n_data échantillons en parallèle avec ProcessPoolExecutor. """
+    args = [(d_in, n_pulse_plateau, len_in, len_out, sensitivity) for _ in range(n_data)]
+
+    with ProcessPoolExecutor() as executor:
+        results = list(executor.map(generate_sample, args))  # On passe une fonction globale et une liste d'args
+
+    # Extraction des résultats
+    input_data, output_data, len_element_output = zip(*results)
+
+    # Conversion en tensors
+    input_data = torch.tensor(input_data, dtype=torch.float)
+    output_data = torch.tensor(output_data, dtype=torch.float)
+    len_element_output = torch.tensor(len_element_output).unsqueeze(-1)
+
+    # Masques
+    arange = torch.arange(len_out + 1).unsqueeze(0).expand(n_data, -1)
+    add_mask = torch.tensor((len_element_output + 1) == arange, dtype=torch.float).unsqueeze(-1)
+    mult_mask = torch.tensor((len_element_output + 1) >= arange, dtype=torch.float).unsqueeze(-1)
+
+    return input_data, output_data, [add_mask, mult_mask]
+
+
+
+def GetData(d_in, n_pulse_plateau, len_in, len_out, n_data_training, n_data_validation, sensitivity, save_path=None, parallel=False):
+    MakeData = MakeData2 if parallel else MakeData1
+
     if save_path is None:
-        return [MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data_validation, sens_fourier, sens_sensor),
-                MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data_training, sens_fourier, sens_sensor)]
+        return [MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data_validation, sensitivity),
+                MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data_training, sensitivity)]
 
     else:
         arg = {'d_in': d_in,
                'n_pulse_plateau': n_pulse_plateau,
                'len_in': len_in,
                'len_out': len_out,
-               'sens_fourier': sens_fourier,
-               'sens_sensor': sens_sensor}
+               'sensitivity': sensitivity}
 
         for file in os.listdir(save_path):
             arg_file = loadXmlAsObj(os.path.join(save_path, file, 'arg.xml'))
@@ -48,7 +93,7 @@ def GetData(d_in, n_pulse_plateau, len_in, len_out, n_data_training, n_data_vali
                 MultMaskTraining = torch.load(os.path.join(save_path, file, 'MultMaskTraining'))
 
                 if len(InputTraining) < n_data_training:
-                    Input, Output, Mask = MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data_training - len(InputTraining), sens_fourier, sens_sensor)
+                    Input, Output, Mask = MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data_training - len(InputTraining), sensitivity)
                     InputTraining = torch.cat((InputTraining, Input), dim=0)
                     OutputTraining = torch.cat((OutputTraining, Output), dim=0)
                     AddMaskTraining = torch.cat((AddMaskTraining, Mask[0]), dim=0)
@@ -64,7 +109,7 @@ def GetData(d_in, n_pulse_plateau, len_in, len_out, n_data_training, n_data_vali
                 MultMaskValidation = torch.load(os.path.join(save_path, file, 'MultMaskValidation'))
 
                 if len(InputValidation) < n_data_validation:
-                    Input, Output, Mask = MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data_validation - len(InputValidation), sens_fourier, sens_sensor)
+                    Input, Output, Mask = MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data_validation - len(InputValidation), sensitivity)
                     InputValidation = torch.cat((InputValidation, Input), dim=0)
                     OutputValidation = torch.cat((OutputValidation, Output), dim=0)
                     AddMaskValidation = torch.cat((AddMaskValidation, Mask[0]), dim=0)
@@ -82,13 +127,13 @@ def GetData(d_in, n_pulse_plateau, len_in, len_out, n_data_training, n_data_vali
         file = 'config' + str(len(os.listdir(save_path)))
         os.mkdir(os.path.join(save_path, file))
         saveObjAsXml(arg, os.path.join(save_path, file, 'arg.xml'))
-        InputValidation, OutputValidation, MaskValidation = MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data_validation, sens_fourier, sens_sensor)
+        InputValidation, OutputValidation, MaskValidation = MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data_validation, sensitivity)
         AddMaskValidation, MultMaskValidation = MaskValidation
         torch.save(InputValidation, os.path.join(save_path, file, 'InputValidation'))
         torch.save(OutputValidation, os.path.join(save_path, file, 'OutputValidation'))
         torch.save(AddMaskValidation, os.path.join(save_path, file, 'AddMaskValidation'))
         torch.save(MultMaskValidation, os.path.join(save_path, file, 'MultMaskValidation'))
-        InputTraining, OutputTraining, MaskTraining = MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data_training, sens_fourier, sens_sensor)
+        InputTraining, OutputTraining, MaskTraining = MakeData(d_in, n_pulse_plateau, len_in, len_out, n_data_training, sensitivity)
         AddMaskTraining, MultMaskTraining = MaskTraining
         torch.save(InputTraining, os.path.join(save_path, file, 'InputTraining'))
         torch.save(OutputTraining, os.path.join(save_path, file, 'OutputTraining'))
@@ -101,4 +146,12 @@ def GetData(d_in, n_pulse_plateau, len_in, len_out, n_data_training, n_data_vali
 
 
 if __name__ == '__main__':
-    I, O, len_el_O = MakeData(10, 5, 30, 40, 100, 0.1, 0.05)
+    import time
+
+    t = time.time()
+    I, O, len_el_O = MakeData(10, 5, 300, 400, 100, 0.1)
+    print(time.time() - t)
+
+    t = time.time()
+    I, O, len_el_O = MakeData2(10, 5, 300, 400, 100, 0.1)
+    print(time.time() - t)
