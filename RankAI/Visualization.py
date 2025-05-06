25d411435fa7e2026214eb1a0af941f70ddb74ea
MatthCorn <matthieucornu@yahoo.fr>
2025-05-06 11:34:56 +0200

    simu

--- RankAI/Visualization.py
+++ RankAI/Visualization.py
@@ -141,7 +141,7 @@ def PlotError(save_path):
     plt.show()
 
 if __name__ == '__main__':
-    save_path = r"C:\Users\Matth\Documents\Projets\RankAI\Save\V4\Vecteurs\2025-04-30__14-58(1)"
+    save_path = r"C:\Users\Matth\Documents\Projets\RankAI\Save\V4\Vecteurs\2025-05-05__19-23"
 
     PlotError(save_path)
 
