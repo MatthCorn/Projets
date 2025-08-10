4085263a9eaa4ec0aed7f752f2f9da626eda0965
MatthCorn <matthieucornu@yahoo.fr>
2025-08-10 20:38:18 +0200

    gg a moi le MRO marche comme je veux

--- Inter/test2.py
+++ Inter/test2.py
@@ -1,41 +1,45 @@
+################################################################################################################################
+# De base, nous avons une classe B qui hérite d'une classe A
 class A:
     def Oi(self):
         return 'A'
 
-class D:
-    def Oi(self):
-        return 'D'
-
-class B(D):
+class B(A):
     def Oi(self):
         return 'B' + super().Oi()
 
-class C(B, A):
-    def Oi(self):
-        return 'C' + super().Oi()
+print(B().Oi()) # BA
+################################################################################################################################
 
-print(C().Oi()) # CBD
-
-class B(D):
-    def Oi(self):
-        return 'B' + super().Oi()
-
-class C(B, A):
-    def Oi(self):
-        return 'C' + super().Oi()
-
-print(C().Oi()) # CBA
-
-class D:
+################################################################################################################################
+class D(A):
     def Oi(self):
         return 'D' + super().Oi()
 
+# On voudrait remplacer que B hérite d'une autre sous-classe de A à la place : D
+# mais on ne veut pas réécrire B comme ici
+
 class B(D):
     def Oi(self):
         return 'B' + super().Oi()
 
-class C(B, A):
-    def Oi(self):
-        return 'C' + super().Oi()
+print(B().Oi()) # CBA
+################################################################################################################################
 
-print(C().Oi()) # CBDA
+
+################################################################################################################################
+# à la place, on écrit la sous-classe B comme n'héritant de personne
+# et on l'utilisera comme template pour la variante B(A) et B(D)
+
+class B:
+    def Oi(self):
+        return 'B' + super().Oi()
+
+class BA(B, A):
+    pass
+
+class BD(B, D):
+    pass
+
+print(BA().Oi()) # BA
+print(BD().Oi()) # BA
