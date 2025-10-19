1db84f99f76bae034702a704faa3d5bbbf661083
MatthCorn <matthieucornu@yahoo.fr>
2025-10-19 12:10:58 +0200

    validation de la méthode de préparation de loptim

--- Optuna/optuna_global.py
+++ Optuna/optuna_global.py
@@ -3,34 +3,80 @@ import subprocess
 import json
 import os
 import uuid
+import time
 
 os.environ["MKL_THREADING_LAYER"] = "GNU"
 
 local = os.path.join(os.path.abspath(__file__)[:(os.path.abspath(__file__).index("Projets"))], "Projets")
-TRAINER_SCRIPT = os.path.join(local, "Inter", "NetworkGlobal", "Trainer.py")
+TRAINER_SCRIPT = {"global": os.path.join(local, "Inter", "NetworkGlobal", "Trainer.py"),
+                  "windowed": os.path.join(local, "Inter", "NetworkGlobalWindowed", "Trainer.py"),
+                  "memory": os.path.join(local, "Inter", "NetworkRecursive", "Trainer.py")}
 
-def objective(trial, RUN_DIR):
+
+def deep_suggest(trial, objet, key=None):
+    try:
+        if isinstance(objet, list):
+            if objet[0] == 'suggest':
+                trialtype, values, *kwargs = objet[1:]
+                kwargs = dict(item for d in kwargs for item in d.items())
+
+                if trialtype == 'categorical':
+                    return trial.suggest_categorical(key, values, **kwargs)
+                if trialtype == 'int':
+                    return trial.suggest_int(key, *values, **kwargs)
+                if trialtype == 'float':
+                    return trial.suggest_float(key, *values, **kwargs)
+            else:
+                return [deep_suggest(trial, x) for x in objet]
+        if isinstance(objet, dict):
+            return {key: deep_suggest(trial, value, key=key) for key, value in objet.items()}
+        return objet
+    except Exception as e:
+        print(e)
+        print(objet)
+        print('oi')
+    if isinstance(objet, list):
+        if objet[0] == 'suggest':
+            trialtype, values, *kwargs = objet[:1]
+            kwargs = dict(item for d in kwargs for item in d.items())
+
+            if trialtype == 'categorical':
+                return trial.suggest_categorical(key, values, **kwargs)
+            if trialtype == 'int':
+                return trial.suggest_int(key, *values, **kwargs)
+            if trialtype == 'float':
+                return trial.suggest_float(key, *values, **kwargs)
+        else:
+            return [deep_suggest(trial, x) for x in objet]
+    if isinstance(objet, dict):
+        return {key: deep_suggest(trial, value, key=key) for key, value in objet.items()}
+    return objet
+
+
+def objective(trial, RUN_DIR, params):
     # Hyperparamètres que l’on veut optimiser
-    params = {
-        "n_encoder": trial.suggest_int("n_encoder", 2, 4),
-        "n_decoder": trial.suggest_int("n_decoder", 2, 4),
-        "d_att": trial.suggest_categorical("d_att", [64, 128,]),
-        "widths_embedding": [trial.suggest_categorical("widths_embedding", [16, 32])],
-        "width_FF": [trial.suggest_categorical("width_FF", [128])],
-        "n_heads": trial.suggest_categorical("n_heads", [2, 4]),
-        "dropout": trial.suggest_float("dropout", 0.0, 0.3),
-        "lr_option": {
-            "value": trial.suggest_loguniform("lr", 1e-6, 1e-3),
-            "reset": "y",
-            "type": "cos"
-        },
-        "weight_decay": trial.suggest_loguniform("weight_decay", 1e-6, 1e-2),
-        "batch_size": trial.suggest_categorical("batch_size", [512, 1000]),
-        "n_iter": 10,  # pour optuna, on réduit un peu pour tester
-        "NDataT": 5000,
-        "NDataV": 100,
-        "period_checkpoint" : -1
-    }
+    # params = {
+    #     "n_encoder": trial.suggest_int("n_encoder", 2, 4),
+    #     "n_decoder": trial.suggest_int("n_decoder", 2, 4),
+    #     "d_att": trial.suggest_categorical("d_att", [64, 128, ]),
+    #     "widths_embedding": trial.suggest_categorical("widths_embedding", [[16], [32]]),
+    #     "width_FF": [trial.suggest_categorical("width_FF", [128])],
+    #     "n_heads": trial.suggest_categorical("n_heads", [2, 4]),
+    #     "dropout": trial.suggest_float("dropout", 0.0, 0.3),
+    #     "lr_option": {
+    #         "value": trial.suggest_loguniform("lr", 1e-6, 1e-3),
+    #         "reset": "y",
+    #         "type": "cos"
+    #     },
+    #     "weight_decay": trial.suggest_loguniform("weight_decay", 1e-6, 1e-2),
+    #     "batch_size": trial.suggest_categorical("batch_size", [512, 1000]),
+    #     "n_iter": 10,  # pour optuna, on réduit un peu pour tester
+    #     "NDataT": 5000,
+    #     "NDataV": 100,
+    #     "period_checkpoint": -1,
+    #     "script": 'global'
+    # }
+    params = deep_suggest(trial, params)
 
     # Nom unique pour le fichier JSON
     json_file = os.path.join(RUN_DIR, f"params_{uuid.uuid4().hex}.json")
@@ -40,27 +86,52 @@ def objective(trial, RUN_DIR):
     # Fichier temporaire pour la progression
     progress_file = os.path.join(RUN_DIR, f"progress_{uuid.uuid4().hex}.txt")
 
-    # Appel du script externe (simulateur d'entraînement)
-    result = subprocess.run(
-        ["python", TRAINER_SCRIPT, json_file, progress_file],
-        capture_output=True,
+    # Lancement du script d'entraînement
+    process = subprocess.Popen(
+        ["python", TRAINER_SCRIPT[params['script']], json_file, progress_file],
         text=True,
+        stdout=None,
+        stderr=None,
         encoding="utf-8"
     )
 
+    # Boucle de surveillance
+    while process.poll() is None:
+        time.sleep(1)  # toutes les 10 secondes
+        if os.path.exists(progress_file):
+            with open(progress_file, "r") as f:
+                lines = f.readlines()
+            if lines:
+                try:
+                    step, score = lines[-1].split()
+                    step = int(step)
+                    score = float(score)
+                    trial.report(score, step)
+
+                    if trial.should_prune():
+                        process.terminate()
+                        raise optuna.TrialPruned()
+                except Exception:
+                    pass
+
+    score = float('inf')
+    if os.path.exists(progress_file):
+        with open(progress_file, "r") as f:
+            lines = f.readlines()
+        if lines:
+            try:
+                step, score = lines[-1].split()
+                score = float(score)
+            except:
+                pass
+
     # Nettoyage
+    stdout, _ = process.communicate()
     os.remove(json_file)
-
-    # Récupérer le score imprimé par trainer_test.py
-    score = float("inf")
-    for line in result.stdout.splitlines():
-        if "Final Error:" in line:
-            score = float(line.split()[-1])
-
-    trial.set_user_attr('message', result.stdout)
+    if os.path.exists(progress_file):
+        os.remove(progress_file)
 
     return score
-
 
 if __name__ == "__main__":
     job_id = os.getenv("SLURM_JOB_ID", str(uuid.uuid4().hex))  # fallback si tu testes en local
@@ -73,15 +144,47 @@ if __name__ == "__main__":
     db_path = os.path.join(run_dir, "optuna.db")
     storage = f"sqlite:///{db_path}"
 
+    params = {
+        "n_encoder": ['suggest', 'int', [2, 4]],
+        "n_decoder": ['suggest', 'int', [2, 4]],
+        "d_att": ['suggest', 'categorical', [64, 128]],
+        "widths_embedding": ['suggest', 'categorical', [[16], [32]]],
+        "width_FF": [128],
+        "n_heads": ['suggest', 'categorical', [2, 4]],
+        "dropout": ['suggest', 'float', [0, 0.3]],
+        "lr_option": {
+            "value": ['suggest', 'float', [1e-6, 1e-3], {'log': True}],
+            "reset": "y",
+            "type": "cos"
+        },
+        "weight_decay": ['suggest', 'float', [1e-6, 1e-2], {'log': True}],
+        "batch_size": ['suggest', 'categorical', [512, 1024]],
+        "n_iter": 10,  # pour optuna, on réduit un peu pour tester
+        "NDataT": 5000,
+        "NDataV": 100,
+        "period_checkpoint": -1,
+        "script": 'global'
+    }
+
+
+
+    try:
+        import sys
+        json_file = sys.argv[1]
+        with open(json_file, "r") as f:
+            temp_param = json.load(f)
+        params.update(temp_param)
+    except:
+        print("nothing loaded")
+
     study = optuna.create_study(
         study_name="distributed-test",
         storage=storage,
         load_if_exists=True,
-        direction="minimize"
+        direction="minimize",
+        sampler=optuna.samplers.TPESampler(),
+        pruner=optuna.pruners.HyperbandPruner()
     )
 
-    def my_callback(study, trial):
-        print(f"{trial.user_attrs.get('message')}")
-
-    study.optimize(lambda x: objective(x, run_dir), n_trials=5, callbacks=[my_callback])
+    study.optimize(lambda x: objective(x, run_dir, params), n_trials=5)
 
