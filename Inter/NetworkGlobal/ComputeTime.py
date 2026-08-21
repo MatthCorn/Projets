523b12e348ae5eeed8443d6886c198991d63c269
MatthCorn <matthieucornu@yahoo.fr>
2026-08-22 01:51:05 +0200

    modif param

--- Inter/NetworkGlobal/ComputeTime.py
+++ Inter/NetworkGlobal/ComputeTime.py
@@ -69,5 +69,5 @@ if __name__ == '__main__':
     len_in = 90
     batch_size = 50
     while batch_size < 200000:
-        test_vitesse(batch_size=2000, len_in=len_in)
+        test_vitesse(batch_size=batch_size, len_in=len_in)
         batch_size = int(batch_size * 1.2)
